5a11c5448d80901e95c6e5b9166f725b4e516f0e
Ahmed Ben Houria <ahmed@vyoo.ai>
2024-10-30 17:06:58 +0100

    Update gradle version (8.9)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,26 +57,26 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.3")
-    implementation ("androidx.compose.runtime:runtime:1.6.8")
-    implementation ("androidx.compose.runtime:runtime-livedata:1.6.8")
-    implementation ("androidx.compose.runtime:runtime-rxjava2:1.6.8")
-    implementation("androidx.activity:activity-compose:1.9.0")
-    implementation(platform("androidx.compose:compose-bom:2024.06.00"))
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
+    implementation ("androidx.compose.runtime:runtime:1.7.4")
+    implementation ("androidx.compose.runtime:runtime-livedata:1.7.4")
+    implementation ("androidx.compose.runtime:runtime-rxjava2:1.7.4")
+    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation(platform("androidx.compose:compose-bom:2024.10.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3:1.2.1")
+    implementation("androidx.compose.material3:material3:1.3.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.06.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.10.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
-    implementation ("androidx.navigation:navigation-compose:2.8.0-beta05")
+    implementation ("androidx.navigation:navigation-compose:2.8.3")
     implementation ("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.0")
 
     /*    // Compose Destinations
